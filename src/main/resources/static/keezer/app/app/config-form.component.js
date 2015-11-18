b14acca8df1d185c75ea03a9fbe15074c3cc6f84
Daniel Giguere <github@danielgiguere.com>
2015-11-18 18:20:51 -0500

    Finally getting current configs work

--- src/main/resources/static/keezer/app/app/config-form.component.js
+++ src/main/resources/static/keezer/app/app/config-form.component.js
@@ -6,14 +6,31 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
         case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
     }
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 var angular2_1 = require('angular2/angular2');
+var http_1 = require('angular2/http');
 var config_1 = require("./config");
 var ConfigFormComponent = (function () {
-    function ConfigFormComponent() {
+    function ConfigFormComponent(http) {
         // TODO: 1. Go get actual values from database.
+        //http.get('/getcurrentkeezerconfig').map(res => res.json()).subscribe(config => this.config = config);
+        //this.http.get('/getcurrentkeezerconfig').map(res => res.json()).subscribe(function (config) {
+        ///  console.log("Successfully received current config: " + config)
+        //});
         // TODO: 2. Load model = new Config(..) with these values.
-        this.model = new config_1.Config('2015-11-11T11:00', 18, 19, 20, 21, '2015-11-20T20:00', 22, 23, 24, 25, '2015-11-21T21:00', 0, 1, 2, 3, '2015-11-22T22:00');
+        // let's feed that with temporary values, until the real values are loaded
+        this.resultsLoaded = false;
+        this.model = new config_1.Config('2015-11-11T11:01', 1, 2, 3, 4, '2015-11-11T11:02', 5, 6, 7, 8, '2015-11-11T11:03', 9, 10, 11, 12, '2015-11-11T11:04');
         this.submitted = false;
+        var self = this;
+        http.get('/getcurrentkeezerconfig').map(function (res) { return res.json(); }).subscribe(function (config) {
+            console.log("Successfully received current config: " + JSON.stringify(config));
+            //this.model = config;
+            self.model = config; //new Config('2015-11-11T11:00', 2224, 19, 20, 21, '2015-11-20T20:00', 22, 23, 24, 25, '2015-11-21T21:00', 0, 1, 2, 3, '2015-11-22T22:00');
+            self.resultsLoaded = true;
+        });
     }
     ConfigFormComponent.prototype.onSubmit = function () {
         // TODO 3: Submit this to server, and display errors if any.
@@ -30,8 +47,10 @@ var ConfigFormComponent = (function () {
         angular2_1.Component({
             selector: 'config-form',
             templateUrl: 'app/config-form.component.html',
-            directives: [angular2_1.CORE_DIRECTIVES, angular2_1.FORM_DIRECTIVES]
-        })
+            directives: [angular2_1.CORE_DIRECTIVES, angular2_1.FORM_DIRECTIVES],
+            viewProviders: [http_1.HTTP_PROVIDERS]
+        }),
+        __param(0, angular2_1.Inject(angular2_1.forwardRef(function () { return http_1.Http; })))
     ], ConfigFormComponent);
     return ConfigFormComponent;
 })();
--- src/main/resources/static/keezer/app/app/config-form.component.ts
+++ src/main/resources/static/keezer/app/app/config-form.component.ts
@@ -1,21 +1,42 @@
-import {Component, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+import {Component, Inject, forwardRef, CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+import {Http, HTTP_PROVIDERS} from 'angular2/http';
 import {Config} from "./config";
+
 
 @Component({
   selector: 'config-form',
   templateUrl: 'app/config-form.component.html',
-  directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
+  directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
+  viewProviders: [HTTP_PROVIDERS]
 })
 export class ConfigFormComponent {
 
+  constructor(@Inject(forwardRef(() => Http)) http) {
+
+    let self = this;
+
+    http.get('/getcurrentkeezerconfig').map(res => res.json()).subscribe(function (config) {
+      console.log("Successfully received current config: " + JSON.stringify(config));
+      //this.model = config;
+      self.model = config; //new Config('2015-11-11T11:00', 2224, 19, 20, 21, '2015-11-20T20:00', 22, 23, 24, 25, '2015-11-21T21:00', 0, 1, 2, 3, '2015-11-22T22:00');
+      self.resultsLoaded = true;
+    });
+  }
+
   // TODO: 1. Go get actual values from database.
-  
+  //http.get('/getcurrentkeezerconfig').map(res => res.json()).subscribe(config => this.config = config);
+  //this.http.get('/getcurrentkeezerconfig').map(res => res.json()).subscribe(function (config) {
+  ///  console.log("Successfully received current config: " + config)
+  //});
 
 
   // TODO: 2. Load model = new Config(..) with these values.
 
+  // let's feed that with temporary values, until the real values are loaded
 
-  model = new Config('2015-11-11T11:00', 18, 19, 20, 21, '2015-11-20T20:00', 22, 23, 24, 25, '2015-11-21T21:00', 0, 1, 2, 3, '2015-11-22T22:00');
+  resultsLoaded = false;
+
+  model = new Config('2015-11-11T11:01', 1, 2, 3, 4, '2015-11-11T11:02', 5, 6, 7, 8, '2015-11-11T11:03', 9, 10, 11, 12, '2015-11-11T11:04');
 
   submitted = false;
 
@@ -28,4 +49,5 @@ export class ConfigFormComponent {
 
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
+
 }
